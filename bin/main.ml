36ba083392d02b59a0ee84841331e671711564d7
Julin Shaji <julinshaji01@gmail.com>
2024-08-20 09:29:09 +0530

    remove some commented code

--- bin/main.ml
+++ bin/main.ml
@@ -22,27 +22,9 @@ let minute = now.Unix.tm_min
 
 (* https://github.com/leostera/minttea/blob/b084ec7401c52167fae5087577133e52e3874899/examples/views/main.ml#L54 *)
 let ref = Riot.Ref.make ()
+
+
 (*
-let init _model =
-  (* let st = time_to_model (gethm ()) in *)
-  Command.Noop
-  (* Command.Seq [ Set_timer (ref, 1.0); Enter_alt_screen ] *)
-  *)
-
-
-  (*
-let fmt model =
-  let hrs = model.hours in
-  let min = model.mins in
-  let sec = model.secs in
-  let pst = model.past in
-  format_of_string 
-  [%string "" ]
-  *)
-
-
-
-  (*
 (* https://github.com/janestreet/ppx_string *)
 let fmt = format_of_string "
 ITLISASAMPM
@@ -80,19 +62,6 @@ hours.(0)
 *)
 (* Printf.sprintf fmt "world" 123;; *)
 
-(*
-let model_to_str m =
-*)
-
-
-(*
-let initial_model = {
-  hours = Array.make 13 false;
-  mins = Array.make 5 false;
-  leds = Array.make 4 false;
-  past = false
-} 
-*)
 
 let initial_model = time_to_model (gethm ())
 
@@ -122,16 +91,8 @@ let update event model =
   (* Ignore other events *)
   | _ -> (model, Command.Noop)
 
-
-let a = "asdf"
-
-let red fmt = Spices.(default |> fg (color "#992222") |> build) fmt
-let bold fmt = Spices.(default |> bold true |> build) fmt
-let bold' fmt = Spices.(default |> bold true)
-
 let view model = 
     build_outstr model
-    (* bold "%s" [%string "%{a}" ] *)
 
 let app = Minttea.app ~init ~update ~view ()
 let () = Minttea.start app ~initial_model
